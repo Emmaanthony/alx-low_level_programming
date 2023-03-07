013a000fadce2953ff363ab9a497e8f0abf28761
Emmanuel Anthony <kcbusiness79@gmail.com>
2023-03-07 13:39:46 -0800

    Update

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,16 +1,12 @@
 #include "main.h"
-#include <stdio.h>
-
-/*
- * _memcpy -  copies bytes from memory area
- * @n: number of bytes
- * @src: first memory area
- * @dest: second memory area
- * return: the  pointer to dest
- * this code is written by Emmanuel Anthony
+/**
+ * _memcpy - copies memory area,
+ * @dest: destination memory area.
+ * @src: source memory area.
+ * @n: bytes filled.
+ * Return: the pointer to dest.
  */
-
-char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
@@ -19,4 +15,3 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 
 	return (dest);
 }
-
