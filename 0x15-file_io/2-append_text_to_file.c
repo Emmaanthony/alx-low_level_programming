d817878c5246052293f94366a99a3dbc020d376b
Emmanuel Anthony <kcbusiness79@gmail.com>
2023-04-07 15:19:02 -0700

    Update

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,20 +7,13 @@
 #include <sys/types.h>
 #include <stddef.h>
 #include <string.h>
-/* @filename: Filename
+/**
+ * append_text_to_file - Appends to a file
+ * @filename: Filename
  * @text_content: String to write to file
  * Return: 1 for success, -1 on failure
  */
 
-
-/* function which takes a filename and string as parameters and 
- * appends the contents of the string to the file
- * It first checks for any errors related to the filename such as a NULL pointer, and 
- * then opens the file in "read/write/append" mode. 
- * Afterwards, it checks for any errors related to the content and 
- * then writes the string to the file. 
- * Finally, it returns 1 if the operation was successful, or -1 on failure.
- */
 
 
 int append_text_to_file(const char *filename, char *text_content)
